35006213259c501edb9f0c223b44fcf4126eba47
Ariel983 <arielsarango98@gmail.com>
2019-10-17 20:33:38 -0500

    cambios en clase introjava.java

---
+++ introjava/src/introjava/Introjava.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package introjava;
+
+public class Introjava {
+
+ 
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+        System.out.println("bienvenido a la programacion en JAVA mijin                                                                   ");
+        // TODO code application logic here
+        
+        
+        
+    }
+    
+}

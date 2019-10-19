09134a72d55b41b9d9610763008c1dccda037d3b
Ariel <arielsarango98@gmial.com>
2019-10-18 19:35:13 -0500

    cambios ralizados

---
+++ introjava/src/introjava/Operadores.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//operadores de las distintas clases en programacion JAVA
+
+package introjava;
+public class Operadores {
+    
+    public static void main(String[] args) {
+        //operadores aritmeticos 
+        int suma = 14+14;
+        int resta = 20-10;
+        double multiplicacion = 50 * 0.12;
+        int div = 7%2;   
+        System.out.println("la suma es:"+ suma);
+        System.out.println("la resta es:"+ resta);
+        System.out.println("la multiplicacoin es:"+ multiplicacion);
+        System.out.println("la division es:"+ div);
+        
+        
+    }   
+}

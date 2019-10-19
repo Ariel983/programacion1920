34cf41628c3caa8162bfdf0956836ab2936d3370
Ariel <arielsarango98@gmial.com>
2019-10-18 19:55:47 -0500

    OPERADORES DE INCREMENTO

--- introjava/src/introjava/Operadores.java
+++ introjava/src/introjava/Operadores.java
@@ -9,15 +9,38 @@ package introjava;
 public class Operadores {
     
     public static void main(String[] args) {
-        //operadores aritmeticos 
+        //OPERADORES ARITMETICOS
         int suma = 14+14;
         int resta = 20-10;
         double multiplicacion = 50 * 0.12;
         int div = 7%2;   
+        System.out.println("OPERADORES ARITMETICOS");
         System.out.println("la suma es:"+ suma);
         System.out.println("la resta es:"+ resta);
         System.out.println("la multiplicacoin es:"+ multiplicacion);
         System.out.println("la division es:"+ div);
+        
+        //OPERDORES DE INCREMENTO
+        System.out.println("OPERADORES DE INCREMENTO");
+        int nota = 15;
+        nota =nota+1;
+        System.out.println("Su onta final: "+nota);
+        System.out.println("/////");
+        nota +=1;
+        System.out.println("la nota final es:" +nota);
+        System.out.println("/////");
+        nota ++;
+        System.out.println("la nota es:" + nota);
+        
+        //DECREMENTO DE LA VARIABLE
+        System.out.println("DECREMENTO  ");
+        nota =nota-1;
+        System.out.println("el decremento es:"+ nota);
+        nota -=1;
+        System.out.println("el sigueinte decrementeo es:"+ nota);
+        nota --;
+        System.out.println("sigues bajando"+ nota);
+        
         
         
     }   
